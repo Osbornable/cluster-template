5fbf3585e1d6ccf71c34f7f25f66b06512b901d1
Osbornable <48108557+Osbornable@users.noreply.github.com>
2019-04-19 20:12:46 -0400

    Update install_linpack.sh

--- install_linpack.sh
+++ install_linpack.sh
@@ -1,6 +1,11 @@
 set -x
 
+sudo apt-get install -y numactl
+
 wget http://registrationcenter-download.intel.com/akdlm/irc_nas/2169/l_lpk_p_10.3.4.007.tgz
 tar zxvf l_lpk_p_10.3.4.007.tgz.1
 cd linpack_10.3.4/benchmarks/linpack 
-./xlinpack_xeon64                                          
+./xlinpack_xeon64  
+
+
+
